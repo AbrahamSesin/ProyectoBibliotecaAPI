7b230bb3ea61e1fbf5aeac527666609e38fd0bc7
AbrahamSesin <sesinho32@gmail.com>
2023-10-24 22:22:36 -0300

    Actualizacion del delete con implementacion de  (try-catch)

--- src/controller.js
+++ src/controller.js
@@ -7,11 +7,24 @@ class LibroController{
         res.json(result);
     }
 
-        //Insertamos un nuevo libro
+        //Insertamos un nuevo libro dar de alta
     async add(req, res) {
         const libro = req.body;
-        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, aniopublicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.aniopublicacion, libro.isbn]);
-        res.json({"Id insertado": result.insertId});
+        const camposValidos = ["nombre", "autor", "categoria", "aniopublicacion", "isbn"];
+        
+        try {
+            // Verificar si se proporcionan campos inválidos
+            const camposInvalidos = Object.keys(libro).filter(campo => !camposValidos.includes(campo));
+            if (camposInvalidos.length > 0) {
+                throw new Error(`Se proporcionaron campos inválidos: ${camposInvalidos.join(", ")}`);
+            }
+                
+            // Insertar el libro en la base de datos
+            const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, aniopublicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.aniopublicacion, libro.isbn]);
+            res.json({"Id insertado": result.insertId});
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 
 
@@ -23,6 +36,7 @@ class LibroController{
         
     }
 
+        //Implementacion de eliminar libro a traves del campo ISBN con implementacion de  (try-catch)
     async delete(req, res) {
         try {
             const libro = req.body;
@@ -41,8 +55,8 @@ class LibroController{
 
     async getOne(req, res){
         const libro = req.body;
-        const id_libro = parseInt(libro.isbn);
-        const [result] = await pool.query(`SELECT * FROM libros WHERE isbn=(?)`, [id_libro]);
+        const isbn_libro = parseInt(libro.isbn);
+        const [result] = await pool.query(`SELECT * FROM libros WHERE isbn=(?)`, [isbn_libro]);
 
         if (result[0] != undefined) {
             res.json(result);
